97626809848ae28c2760df1a15130d96e1ce9a50
Gustavo <gustavofucitalo1@gmail.com>
2024-01-15 18:09:20 -0300

    add ap1/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,7 +3,7 @@ import database from "../../../../infra/database.js";
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;");
   console.log(result.rows);
-  response.status(200).json({ chave: "São acima da média" });
+  response.status(200).json({ chave: "Fala, arabio saudito" });
 }
 
 export default status;
