58f3d6ba158adffa0ef30d760427e99f7b6d2b06
Gustavo <gustavofucitalo1@gmail.com>
2024-02-20 17:56:27 -0300

    make database more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -14,7 +14,7 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    consonle.error(error);
+    console.error(error);
   } finally {
     await client.end();
   }
