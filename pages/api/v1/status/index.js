37133c5ee24dd33653e8085c9f39bebe5679a1b0
Gustavo <gustavofucitalo1@gmail.com>
2024-02-29 18:46:16 -0300

    finish /api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -13,12 +13,12 @@ async function status(request, response) {
     databaseMaxConnectionsResult.rows[0].max_connections;
 
   const databaseName = process.env.POSTGRES_DB;
-  const databaseOpenedConnectionResult = await database.query({
+  const databaseOpenedConnectionsResult = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
     values: [databaseName],
   });
   const databaseOpenedConnectionsValue =
-    databaseOpenedConnectionResult.rows[0].count;
+    databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
